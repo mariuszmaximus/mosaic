3e1a69fae2c18639ba465a2e10535489f8052782
Dominic Meiser <dmeiser79@gmail.com>
2016-08-03 21:00:28 -0600

    Fix test name.

--- tests/mosaicview_test/mosaicview_test.cpp
+++ tests/mosaicview_test/mosaicview_test.cpp
@@ -1,7 +1,7 @@
 #include <gtest/gtest.h>
 #include <mosaicview.h>
 
-TEST(MosaicView, CanBeConstructed) {
+TEST(MoMosaicView, CanBeConstructed) {
     MoMosaicView* view = new MoMosaicView;
     ASSERT_NE(view, nullptr);
     delete view;
