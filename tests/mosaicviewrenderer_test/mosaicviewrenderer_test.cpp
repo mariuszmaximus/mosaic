3bef831d8638bf1f07dd467951ae6d0d9d48079a
Dominic Meiser <dmeiser79@gmail.com>
2016-08-06 12:19:47 -0600

    Add setup of opengl context.

--- tests/mosaicviewrenderer_test/mosaicviewrenderer_test.cpp
+++ tests/mosaicviewrenderer_test/mosaicviewrenderer_test.cpp
@@ -1,6 +1,7 @@
 #include <gtest/gtest.h>
 #include <mosaicviewrenderer.h>
 #include <QOffscreenSurface>
+#include <QGuiApplication>
 
 
 TEST(MoMosaicViewRenderer, CanBeConstructed) {
@@ -22,4 +23,31 @@ TEST(MoMosaicViewRenderer, CanInitializeGL) {
     surface.create();
     context.makeCurrent(&surface);
     ASSERT_NO_THROW(view.initGL());
+}
+
+int main(int argc, char **argv) {
+    ::testing::InitGoogleTest(&argc, argv);
+
+    QGuiApplication app(argc, argv);
+    Q_UNUSED(app);
+
+    QSurfaceFormat format;
+    format.setMajorVersion(3);
+    format.setMinorVersion(3);
+
+    QOpenGLContext context;
+    context.setFormat(format);
+    context.create();
+    if (!context.isValid()) return 1;
+    qDebug() << QString::fromLatin1("Context created.");
+
+    QOffscreenSurface surface;
+    surface.setFormat(format);
+    surface.create();
+    if(!surface.isValid()) return 2;
+    qDebug() << QString::fromLatin1("Surface created.");
+
+    context.makeCurrent(&surface);
+
+    return RUN_ALL_TESTS();
 }
