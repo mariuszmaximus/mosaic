a3f06f5c422e186cebecb492e6eebca83826d2ac
Dominic Meiser <dmeiser79@gmail.com>
2016-08-06 17:38:47 -0600

    Add widths and heights to mosaicmodel.

--- mosaic_engine/mosaicmodel.cpp
+++ mosaic_engine/mosaicmodel.cpp
@@ -8,6 +8,8 @@ MoMosaicModel::MoMosaicModel() : size_(0) {
 
 void MoMosaicModel::resize(int size) {
     size_ = size;
+    widths.resize(size);
+    heights.resize(size);
     x.resize(size);
     y.resize(size);
     rotations.resize(size);
@@ -16,6 +18,34 @@ void MoMosaicModel::resize(int size) {
 
 int MoMosaicModel::size() const {
     return size_;
+}
+
+void MoMosaicModel::setWidths(const float *widthsBegin,
+                               const float *widthsEnd) {
+    if (std::distance(widthsBegin, widthsEnd) != size_) {
+        throw std::runtime_error(
+                    "Number of elements in [widthsBegin, widthsEnd) "
+                    "doesn't match size of MoMosaicModel.");
+    }
+    std::copy(widthsBegin, widthsEnd, widths.begin());
+}
+
+void MoMosaicModel::getWidths(float *widthsBegin) const {
+    std::copy(widths.cbegin(), widths.cend(), widthsBegin);
+}
+
+void MoMosaicModel::setHeights(const float *heightsBegin,
+                               const float *heightsEnd) {
+    if (std::distance(heightsBegin, heightsEnd) != size_) {
+        throw std::runtime_error(
+                    "Number of elements in [heightsBegin, heightsEnd) "
+                    "doesn't match size of MoMosaicModel.");
+    }
+    std::copy(heightsBegin, heightsEnd, heights.begin());
+}
+
+void MoMosaicModel::getHeights(float *heightsBegin) const {
+    std::copy(heights.cbegin(), heights.cend(), heightsBegin);
 }
 
 void MoMosaicModel::setXCoords(const float *xBegin, const float *xEnd) {
@@ -27,9 +57,10 @@ void MoMosaicModel::setXCoords(const float *xBegin, const float *xEnd) {
     std::copy(xBegin, xEnd, x.begin());
 }
 
-void MoMosaicModel::getXCoords(float *xBegin) {
+void MoMosaicModel::getXCoords(float *xBegin) const {
     std::copy(x.cbegin(), x.cend(), xBegin);
 }
+
 
 void MoMosaicModel::setYCoords(const float *yBegin, const float *yEnd) {
     if (std::distance(yBegin, yEnd) != size_) {
@@ -40,7 +71,7 @@ void MoMosaicModel::setYCoords(const float *yBegin, const float *yEnd) {
     std::copy(yBegin, yEnd, y.begin());
 }
 
-void MoMosaicModel::getYCoords(float *yBegin) {
+void MoMosaicModel::getYCoords(float *yBegin) const {
     std::copy(y.cbegin(), y.cend(), yBegin);
 }
 
@@ -54,7 +85,7 @@ void MoMosaicModel::setRotations(const float *rotationsBegin,
     std::copy(rotationsBegin, rotationsEnd, rotations.begin());
 }
 
-void MoMosaicModel::getRotations(float *rotationsBegin) {
+void MoMosaicModel::getRotations(float *rotationsBegin) const {
     std::copy(rotations.cbegin(), rotations.cend(), rotationsBegin);
 }
 
@@ -68,6 +99,6 @@ void MoMosaicModel::setScales(const float *scalesBegin,
     std::copy(scalesBegin, scalesEnd, scales.begin());
 }
 
-void MoMosaicModel::getScales(float *scalesBegin) {
+void MoMosaicModel::getScales(float *scalesBegin) const {
     std::copy(scales.cbegin(), scales.cend(), scalesBegin);
 }
--- mosaic_engine/mosaicmodel.h
+++ mosaic_engine/mosaicmodel.h
@@ -11,19 +11,32 @@ public:
     void resize(int size);
     int size() const;
 
+    void setWidths(const float* widthsBegin,
+                   const float* widthsEnd);
+    void getWidths(float* widthsBegin) const;
+
+    void setHeights(const float* heightsBegin,
+                    const float* heightsEnd);
+    void getHeights(float* heightsBegin) const;
+
     void setXCoords(const float* xBegin, const float* xEnd);
-    void getXCoords(float* xBegin);
+    void getXCoords(float* xBegin) const;
+
     void setYCoords(const float* yBegin, const float* yEnd);
-    void getYCoords(float* yBegin);
+    void getYCoords(float* yBegin) const;
+
     void setRotations(const float* rotationsBegin,
                       const float* rotationsEnd);
-    void getRotations(float* rotationsBegin);
+    void getRotations(float* rotationsBegin) const;
+
     void setScales(const float* scalesBegin,
                       const float* scalesEnd);
-    void getScales(float* scalesBegin);
+    void getScales(float* scalesBegin) const;
 
 private:
     int size_;
+    std::vector<float> widths;
+    std::vector<float> heights;
     std::vector<float> x;
     std::vector<float> y;
     std::vector<float> rotations;
