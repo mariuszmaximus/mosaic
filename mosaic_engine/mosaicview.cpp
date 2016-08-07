33dadc4d45833cddfece06963ff9740c6c9e11c4
Dominic Meiser <dmeiser79@gmail.com>
2016-08-06 18:14:47 -0600

    Add renderer to MoMosaicView.

--- mosaic_engine/mosaicview.cpp
+++ mosaic_engine/mosaicview.cpp
@@ -1,4 +1,21 @@
 #include <mosaicview.h>
 
-MoMosaicView::MoMosaicView() {
+#include <mosaicviewrenderer.h>
+
+
+MoMosaicView::MoMosaicView() :
+renderer_(new MoMosaicViewRenderer) {
+}
+
+MoMosaicView::~MoMosaicView() {
+    delete renderer_;
+}
+
+
+void MoMosaicView::setModel(std::shared_ptr<MoMosaicModel> model) {
+    renderer_->setModel(model);
+}
+
+std::shared_ptr<MoMosaicModel> MoMosaicView::getModel() const {
+    return renderer_->getModel();
 }
--- mosaic_engine/mosaicview.h
+++ mosaic_engine/mosaicview.h
@@ -2,17 +2,30 @@
 #define MOSAICVIEW_H
 
 #include <QtQuick/QQuickItem>
+#include <memory>
+
+class MoMosaicModel;
+class MoMosaicViewRenderer;
 
 
 class MoMosaicView : public QQuickItem {
     Q_OBJECT
 public:
     MoMosaicView();
-    ~MoMosaicView() {}
+    ~MoMosaicView();
+    void setModel(std::shared_ptr<MoMosaicModel> model);
+    std::shared_ptr<MoMosaicModel> getModel() const;
 
 signals:
 
 public slots:
+private:
+    bool initialized;
+    MoMosaicViewRenderer* renderer_;
+
+    void initGL();
+
+    Q_DISABLE_COPY(MoMosaicView)
 };
 
 #endif // MOSAICVIEW_H
