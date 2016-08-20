6276a9c5217775fc0541f7fd7bb13402de4bf95f
Dominic Meiser <dmeiser79@gmail.com>
2016-08-19 19:19:59 -0600

    Fire off update loop in a thread.

--- mosaic_engine/maindriver.cpp
+++ mosaic_engine/maindriver.cpp
@@ -3,12 +3,27 @@
 #include <QAbstractListModel>
 #include <QString>
 #include <QDebug>
+#include <QThread>
 #include <sourceimages.h>
 #include <algorithm>
 
 #include <tile.h>
 #include <targetimage.h>
 
+struct EvolutionRunner : public QThread {
+    EvolutionRunner(MoMosaicEvolution* evolution) : evolution_(evolution) {}
+    virtual void run() {
+        qDebug() << "in runner.run().";
+        int i = 0;
+        while (1) {
+            qDebug() << "Taking step" << i;
+            evolution_->takeStep();
+            ++i;
+        }
+    }
+
+    MoMosaicEvolution* evolution_;
+};
 
 MoMainDriver::MoMainDriver(QObject *parent) :
     QObject(parent),
@@ -39,6 +54,10 @@ void MoMainDriver::start(QUrl targetUrl) {
     qDebug() << "tiles.size() == " << tiles.size();
 
     evolution_.constructInitialState(targetImage, tiles);
+    EvolutionRunner* runner = new EvolutionRunner(&evolution_);
+    qDebug() << "before runner start";
+    // For now we just leak this thread
+    runner->start();
 }
 
 std::vector<QString> MoMainDriver::getFileNames(
