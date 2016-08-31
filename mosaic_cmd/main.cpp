7cdbaf56bc4f49374a8b687856f4f409fbe330eb
Dominic Meiser <dmeiser79@gmail.com>
2016-08-30 20:45:19 -0600

    Add command line arguments.

--- mosaic_cmd/main.cpp
+++ mosaic_cmd/main.cpp
@@ -1,3 +1,17 @@
+#include <QCommandLineParser>
+#include <QCommandLineOption>
+
+
 int main(int argc, char *argv[]) {
-    return 0;
+    QCoreApplication app(argc, argv);
+
+    QCommandLineParser parser;
+    parser.setApplicationDescription("MoMosaic");
+    parser.addHelpOption();
+    parser.addVersionOption();
+    parser.addOption({"t", "The target picture.", "target_picture"});
+    parser.addOption({"s", "The mosaic tile pictures.", "source_pictures"});
+    parser.process(app);
+
+    return app.exec();
 }
