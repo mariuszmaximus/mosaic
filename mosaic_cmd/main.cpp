1793ecc4437816547112846bfe51c6862729ff62
Dominic Meiser <dmeiser79@gmail.com>
2016-08-30 21:06:42 -0600

    Parse target files and source files from command line.

--- mosaic_cmd/main.cpp
+++ mosaic_cmd/main.cpp
@@ -1,17 +1,28 @@
+#include <QtGlobal>
+#include <QtDebug>
+#include <QGuiApplication>
 #include <QCommandLineParser>
 #include <QCommandLineOption>
 
 
 int main(int argc, char *argv[]) {
-    QCoreApplication app(argc, argv);
+    QGuiApplication app(argc, argv);
+    QGuiApplication::setApplicationName("mosaic_cmd");
+    QGuiApplication::setApplicationVersion("0.0.1");
 
     QCommandLineParser parser;
     parser.setApplicationDescription("MoMosaic");
     parser.addHelpOption();
     parser.addVersionOption();
-    parser.addOption({"t", "The target picture.", "target_picture"});
-    parser.addOption({"s", "The mosaic tile pictures.", "source_pictures"});
+    parser.addOption({"t", "The target picture.", "targetFileName"});
+    parser.addOption({"s", "The mosaic tile pictures.", "sourceFileNames"});
     parser.process(app);
+
+    QString targetFileName = parser.value("t");
+    qInfo() << "Using target image: " << targetFileName;
+
+    QString sourceFileNames = parser.value("s");
+    qInfo() << "Using source files: " << sourceFileNames;
 
     return app.exec();
 }
