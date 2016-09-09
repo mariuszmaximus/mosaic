8e28353f054c68d73c52f2cb7715702ac2e7c509
Dominic Meiser <dmeiser79@gmail.com>
2016-09-08 20:33:21 -0600

    Add rotation of tiles to outline shader.
    Closes #3.

--- mosaic_engine/vshader_outline.glsl
+++ mosaic_engine/vshader_outline.glsl
@@ -3,6 +3,7 @@ in float x;
 in float y;
 in float width;
 in float height;
+in float rotation;
 uniform float targetWidth = 1.0f;
 uniform float targetHeight = 1.0f;
 uniform float numTiles = 10.0f;
@@ -17,8 +18,12 @@ const vec2 pos[] = vec2[4](
 void main(void)
 {
     vec2 offset = pos[gl_VertexID];
-    gl_Position = vec4((x + width * offset.x) / targetWidth,
-                       (y + height * offset.y) / targetHeight,
+    float c = cos(rotation);
+    float s = sin(rotation);
+    float x_ = (x + width * offset.x) / targetWidth;
+    float y_ = (y + height * offset.y) / targetHeight;
+    gl_Position = vec4(c * x_ - s * y_,
+                       s * x_ + c * y_,
                        (gl_InstanceID - numTiles) / numTiles,
                        1.0f);
 }
