68c144d58a6831eec07c61a11bff6c75a80dcb89
Dominic Meiser <dmeiser79@gmail.com>
2016-09-25 19:54:39 -0600

    Initialize array texture from tiles.

--- mosaic_engine/mosaicrenderer.cpp
+++ mosaic_engine/mosaicrenderer.cpp
@@ -155,14 +155,30 @@ void MoMosaicRenderer::synchronize(QQuickFramebufferObject *item) {
 
     // Copy the tiles to textures
     const std::vector<MoTile>& tiles = model_.getTiles();
-    if (!tileTextures_.isCreated()) {
+    if (!tileTextures_.isCreated() && !tiles.empty()) {
         tileTextures_.setMipLevels(1);
         tileTextures_.setLayers(model_.size());
         tileTextures_.setWrapMode(QOpenGLTexture::ClampToEdge);
-        tileTextures_.setMinificationFilter(QOpenGLTexture::LinearMipMapLinear);
+        tileTextures_.setMinificationFilter(QOpenGLTexture::Linear);
         tileTextures_.setMagnificationFilter(QOpenGLTexture::Linear);
-        for (auto& t : tiles) {
-            Q_UNUSED(t);
+        tileTextures_.setFormat(QOpenGLTexture::RGBA8U);
+        static const int maxSize = 256;
+        tileTextures_.setSize(maxSize, maxSize);
+        MO_CHECK_GL_ERROR;
+
+        tileTextures_.allocateStorage();
+        MO_CHECK_GL_ERROR;
+
+        // Now upload the textures
+        for (size_t i = 0; i < tiles.size(); ++i) {
+            const MoTile& tile = tiles[i];
+            QImage tileImage = tile.getImage()->scaled(maxSize, maxSize,
+                                                       Qt::KeepAspectRatio,
+                                                       Qt::FastTransformation);
+            tileImage = tileImage.convertToFormat(QImage::Format_RGBA8888);
+            tileTextures_.setData(0, i, QOpenGLTexture::RGBA_Integer,
+                                  QOpenGLTexture::UInt32_RGBA8,
+                                  (void*)tileImage.bits());
         }
     }
 }
