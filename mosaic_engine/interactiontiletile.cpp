3ec06d72c2bc499b19e77aaae1847df9a6bd1043
Dominic Meiser <dmeiser79@gmail.com>
2016-10-07 21:34:11 -0600

    Skeleton for quadrature loops for integrals over tiles.

--- mosaic_engine/interactiontiletile.cpp
+++ mosaic_engine/interactiontiletile.cpp
@@ -1,5 +1,6 @@
 #include <interactiontiletile.h>
 #include <mosaicmodel.h>
+#include <quadraturerule.h>
 
 #include <QtGlobal>
 
@@ -26,5 +27,26 @@ float MoInteractionTileTile::computeBadnessPair(const MoMosaicModel& model,
     Q_UNUSED(targetImage);
     Q_UNUSED(i);
     Q_UNUSED(j);
+    static const int order = 3;
+    static const float* weights = &MoQuadratureRule[order][MO_QUADRATURE_WEIGHTS][0];
+    static const float* nodes = &MoQuadratureRule[order][MO_QUADRATURE_NODES][0];
+    Q_UNUSED(nodes);
+
+    float badness = 0.0f;
+    for (int ix = 0; ix < order; ++ix) {
+        float b = 0.0f;
+        for (int iy = 0; iy < order; ++iy) {
+            float bp = 0.0f;
+            for (int jx = 0; jx < order; ++jx) {
+                float bpp = 0.0f;
+                for (int jy = 0; jy < order; ++jy) {
+                    bpp += weights[jy] * 1.0f;
+                }
+                bp += weights[jx] * bpp;
+            }
+            b += weights[iy] * bp;
+        }
+        badness += weights[ix] * b;
+    }
     return 1.0f;
 }
