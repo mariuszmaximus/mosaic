7e720916b081f8bcbb552795bd4bfee3a9c7f9dc
Dominic Meiser <dmeiser79@gmail.com>
2016-10-19 20:32:23 -0600

    Add a couple of Lennard-Jones interaction unit tests.
    The first test verifies that the interaction potential is positive when the
    tiles are too clase to oneanother.  The second test verifies that the potential
    is negative when the tiles are separated by more than the range of the
    potential.

--- tests/interactiontiletile_test/lennardjonesintegration_test.cpp
+++ tests/interactiontiletile_test/lennardjonesintegration_test.cpp
@@ -19,35 +19,68 @@ static MoMosaicModel createSomeModel(int numTiles) {
     model.setXCoords(&y[0], &y[0] + numTiles);
     std::vector<float> rotations(numTiles, 0.0f);
     model.setRotations(&rotations[0], &rotations[0] + numTiles);
-    std::vector<float> scales(numTiles, 0.0f);
-    model.setRotations(&scales[0], &scales[0] + numTiles);
+    std::vector<float> scales(numTiles, 1.0f);
+    model.setScales(&scales[0], &scales[0] + numTiles);
     return model;
 }
 
 struct TileTileLennardJones : public ::testing::Test {
     TileTileLennardJones() :
-        epsilon{3.4f},
-        sigma{2.3f},
         model(createSomeModel(2)),
-        interaction(std::unique_ptr<MoPotential>(
+        interaction(new MoInteractionTileTile(
+                        std::unique_ptr<MoPotential>(
                         new MoPotentialLennardJones(
-                            MoLennardJones(epsilon, sigma))))
+                            MoLennardJones(3.4f, 2.3f)))))
     {}
     virtual ~TileTileLennardJones() {}
     virtual void SetUp() {}
     virtual void TearDown() {}
-    const float epsilon;
-    const float sigma;
+    void setLennardJones(const MoLennardJones& lj) {
+        interaction.reset(new MoInteractionTileTile(
+                        std::unique_ptr<MoPotential>(
+                        new MoPotentialLennardJones(lj))));
+    }
     MoMosaicModel model;
-    MoInteractionTileTile interaction;
+    std::unique_ptr<MoInteractionTileTile> interaction;
 };
 
 
 TEST_F(TileTileLennardJones, IsNonZero) {
-    EXPECT_NE(0, interaction.computeBadness(model, model.getTargetImage()));
+    EXPECT_NE(0, interaction->computeBadness(model, model.getTargetImage()));
 }
 
 TEST_F(TileTileLennardJones, IsFinite) {
-    float badness = interaction.computeBadness(model, model.getTargetImage());
+    float badness = interaction->computeBadness(model, model.getTargetImage());
     EXPECT_TRUE(std::isfinite(badness));
+}
+
+TEST_F(TileTileLennardJones, IsPositiveWhenTilesClose) {
+    std::vector<float> widths(model.size());
+    float* x = model.getXCoords();
+    x[0] = 0.0f;
+    x[1] = 0.5f * widths[1];
+    float* y = model.getXCoords();
+    y[0] = 0.0f;
+    y[1] = 0.0f;
+
+    setLennardJones(MoLennardJones(1.0f, 1.0f));
+    float badness = interaction->computeBadness(model, model.getTargetImage());
+    EXPECT_GT(badness, 0.0f);
+}
+
+TEST_F(TileTileLennardJones, IsNegativeWhenTilesSeparated) {
+    const float sigma = 10.0f;
+    std::vector<float> widths(model.size());
+    model.getWidths(&widths[0]);
+    float* x = model.getXCoords();
+    x[0] = 0.0f;
+    // Make tiles non-overlapping
+    x[1] = 0.5f * (widths[0] + widths[1]) + sigma;
+    float* y = model.getYCoords();
+    y[0] = 0.0f;
+    y[1] = 0.0f;
+
+    setLennardJones(MoLennardJones(1.0f, sigma));
+    float badness = interaction->computeBadness(model, model.getTargetImage());
+    EXPECT_LT(badness, 0.0f);
 }
