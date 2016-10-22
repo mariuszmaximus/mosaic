f5d9a918b2e96117a9a91a3252e104a136dffb60
Dominic Meiser <dmeiser79@gmail.com>
2016-10-22 12:03:56 -0600

    Add a non-trivial test for tiletargetcorrelation.

--- tests/tiletargetcorrelation_test/tiletargetcorrelation_test.cpp
+++ tests/tiletargetcorrelation_test/tiletargetcorrelation_test.cpp
@@ -2,6 +2,7 @@
 #include <tiletargetcorrelation.h>
 #include <targetimage.h>
 #include <mosaicmodel.h>
+#include <test_utilities.h>
 
 #include <memory>
 
@@ -21,6 +22,30 @@ TEST(MoTileTargetCorrelation, OfEmptyModelIsZero) {
     MoTargetImage targetImage(QImage(), QSize(40, 30));
     EXPECT_FLOAT_EQ(0.0f,
                     tileTargetCorrelation.computeBadness(model, targetImage));
+}
+
+static MoMosaicModel createSomeModel(const MoTargetImage& targetImage,
+                                     int numTiles) {
+    MoMosaicModel model;
+    std::vector<MoTile> tiles(numTiles,
+                              MoTile(QImage(20, 30, QImage::Format_RGBA8888)));
+    model.constructInitialState(targetImage, tiles);
+    std::vector<float> x(numTiles);
+    model.setXCoords(&x[0], &x[0] + numTiles);
+    std::vector<float> y(numTiles);
+    model.setXCoords(&y[0], &y[0] + numTiles);
+    std::vector<float> rotations(numTiles, 0.0f);
+    model.setRotations(&rotations[0], &rotations[0] + numTiles);
+    std::vector<float> scales(numTiles, 1.0f);
+    model.setScales(&scales[0], &scales[0] + numTiles);
+    return model;
+}
+
+TEST(MoTileTargetCorrelation, OfNonEmptyModelIsNonZero) {
+    MoTileTargetCorrelation tileTargetCorrelation(10);
+    MoTargetImage targetImage(createImage(120, 80), QSize(120, 80));
+    MoMosaicModel model{createSomeModel(targetImage, 3)};
+    EXPECT_NE(0.0f, tileTargetCorrelation.computeBadness(model, targetImage));
 }
 
 
